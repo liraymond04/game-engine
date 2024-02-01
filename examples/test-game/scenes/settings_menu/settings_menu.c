99ce74033daba7291b7aba6b346118daaf941d62
Raymond Li <li.raymond04@gmail.com>
2024-01-31 23:47:25 -0800

    Add references to engine instance in settings test scene

--- examples/test-game/scenes/settings_menu/settings_menu.c
+++ examples/test-game/scenes/settings_menu/settings_menu.c
@@ -3,27 +3,27 @@
 float x = 20, y = 20;
 float speed = 2.0f;
 
-EXPORT void Init() {
+EXPORT void Init(Engine_t *engine) {
     // Initialization logic for the main menu
 }
 
-EXPORT void ProcessInput() {
+EXPORT void ProcessInput(Engine_t *engine) {
     if (IsKeyDown(KEY_W)) y -= speed;
     if (IsKeyDown(KEY_S)) y += speed;
     if (IsKeyDown(KEY_A)) x -= speed;
     if (IsKeyDown(KEY_D)) x += speed;
 }
 
-EXPORT void Update() {
+EXPORT void Update(Engine_t *engine) {
     // Update logic for the main menu
     printf("Settings menu game update!\n");
 }
 
-EXPORT void Draw() {
+EXPORT void Draw(Engine_t *engine) {
     // Drawing logic for the main menu
     DrawRectangle(x, y, 20, 20, BLUE);
 }
 
-EXPORT void Cleanup() {
+EXPORT void Cleanup(Engine_t *engine) {
     // Cleanup logic for the main menu
 }
--- examples/test-game/scenes/settings_menu/settings_menu.h
+++ examples/test-game/scenes/settings_menu/settings_menu.h
@@ -3,14 +3,14 @@
 
 #include "engine.h"
 
-EXPORT void Init();
+EXPORT void Init(Engine_t *engine);
 
-EXPORT void ProcessInput();
+EXPORT void ProcessInput(Engine_t *engine);
 
-EXPORT void Update();
+EXPORT void Update(Engine_t *engine);
 
-EXPORT void Draw();
+EXPORT void Draw(Engine_t *engine);
 
-EXPORT void Cleanup();
+EXPORT void Cleanup(Engine_t *engine);
 
 #endif // !SCENE_SETTINGS_MENU_H
